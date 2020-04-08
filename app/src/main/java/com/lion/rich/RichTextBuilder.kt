53b26c14168cccda2f489a58f25c75080bedd96c
tanglinbing <tanglinbing@4399inc.com>
2020-04-08 12:19:36 +0800

    多种图片加载支持

--- app/src/main/java/com/lion/rich/RichTextBuilder.kt
+++ app/src/main/java/com/lion/rich/RichTextBuilder.kt
@@ -3,6 +3,7 @@ package com.lion.rich
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.text.SpannableString
 import android.text.SpannableStringBuilder
 import android.text.style.ImageSpan
@@ -36,30 +37,34 @@ class RichTextBuilder(private val textView: TextView) {
         }.build())
     }
 
-    fun newline() = +"\n"
+    fun newline(num: Int = 1) = repeat(num) { +"\n" }
 
-    fun img(url: String, block: (ImgOption.() -> Unit)? = null) {
-        val si = SpanInfo(text.length, text.length + LEN)
+    fun img(url: String, block: (ImgOption.() -> Unit)? = null) = img {
+        createImageSpanByUrl(url, block)
+    }
+
+    fun img(bitmap: Bitmap) = img {
+        ImageSpan(context, bitmap)
+    }
+
+    fun img(drawable: Drawable) = img {
+        check(drawable)
+        ImageSpan(drawable)
+    }
+
+    fun img(resId: Int) = img {
+        val drawable = context.getDrawable(resId)!!
+        check(drawable)
+        ImageSpan(drawable)
+    }
+
+    private fun img(creator: suspend () -> ImageSpan) {
+        val start = text.length
         +IMG
+        val si = SpanInfo(start, start + LEN)
         GlobalScope.launch(Dispatchers.Main) {
-            val bitmap = withContext(Dispatchers.IO) {
-                val b = BitmapFactory.decodeStream(URL(url).openStream())
-                block ?: return@withContext b
-                return@withContext ImgOption().apply(block).run {
-                    if (width <= 0 && height <= 0) return@run b
-                    if (keepRatio) {
-                        if (width > 0) {
-                            height = width * b.height / b.width
-                        } else {
-                            width = height * b.width / b.height
-                        }
-                    }
-                    return@run Bitmap.createScaledBitmap(b, width, height, false)
-                }
-            }
-
             text.setSpan(
-                ImageSpan(context, bitmap),
+                creator(),
                 si.start,
                 si.end,
                 SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
@@ -68,6 +73,33 @@ class RichTextBuilder(private val textView: TextView) {
         }
     }
 
+    private suspend fun createImageSpanByUrl(
+        url: String,
+        block: (ImgOption.() -> Unit)? = null
+    ): ImageSpan {
+        val bitmap = withContext(Dispatchers.IO) {
+            val b = BitmapFactory.decodeStream(URL(url).openStream())
+            block ?: return@withContext b
+            return@withContext ImgOption().apply(block).run {
+                if (width <= 0 && height <= 0) return@run b
+                if (keepRatio) {
+                    if (width > 0) {
+                        height = width * b.height / b.width
+                    } else {
+                        width = height * b.width / b.height
+                    }
+                }
+                return@run Bitmap.createScaledBitmap(b, width, height, false)
+            }
+        }
+        return ImageSpan(context, bitmap)
+    }
+
+    private fun check(drawable: Drawable) {
+        val b = drawable.bounds
+        if (b.bottom <= b.top || b.right <= b.left)
+            drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+    }
 
     private fun append(cs: CharSequence, opt: Options? = null) {
         text.append(cs)
